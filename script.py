6ec481200f2a7bce5ff2ced69e7ded610cf9630b
Amir <alnomani@protonmailcom>
2017-10-31 14:50:18 +0100

    Rename test

---
+++ script.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def costFunction(predicted, y):
+	return 1/2*m *sum((y - predicted)**2)
+
+def h(thetas,featureMatrix):
+	repeatedThetas = np.tile(thetas,(len(featureMatrix),1))
+	interceptVector  = repeatedThetas[:,0]
+	return (repeatedThetas*featureMatrix) + interceptVector
+#git test
+def LRcls(k):
+	thetas = np.zeros(k)
+	alpha = 0.0001
+	hVector = h(thetas, featureMatrix)
+	
+	for gradient > alpha:
+		newThetas = thetas[0] - alpha*(1/m)*sum(hVector-y)
+	
+ik wil kokosnoot eten
+
+
+	
